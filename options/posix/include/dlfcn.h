fc243da3aab5ce8203b5af2a2611cca597874ca2
NetBSD User <netbsduser@users.noreply.github.com>
2024-10-11 21:22:49 +0100

    options/posix: need dl defs when building with glibc option

--- options/posix/include/dlfcn.h
+++ options/posix/include/dlfcn.h
@@ -34,7 +34,7 @@ void *dlvsym(void *__restrict __handle, const char *__restrict __name, const cha
 
 #endif /* !__MLIBC_ABI_ONLY */
 
-#if defined(_GNU_SOURCE) && __MLIBC_GLIBC_OPTION
+#if ((defined(_GNU_SOURCE) || defined(__MLIBC_BUILDING_MLIBC)) && __MLIBC_GLIBC_OPTION)
 
 //gnu extension
 typedef struct {
